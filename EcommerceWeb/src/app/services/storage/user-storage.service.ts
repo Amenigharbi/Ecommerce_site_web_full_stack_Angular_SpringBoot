b9f8722b40f7910d9cb6e962ad5a49aeb5a6ae21
ameni <gharbiameni17@gmail.com>
2024-02-28 21:47:02 +0100

    updates

--- EcommerceWeb/src/app/services/storage/user-storage.service.ts
+++ EcommerceWeb/src/app/services/storage/user-storage.service.ts
@@ -17,4 +17,27 @@ export class UserStorageService {
     window.localStorage.removeItem(USER);
     window.localStorage.setItem(USER,JSON.stringify(user));
   }
+  static getToken():string{
+    return localStorage.getItem(TOKEN);
+  }
+  static getUser():string{
+    return JSON.parse(localStorage.getItem(USER));
+  }
+
+  static getUserId():string {
+    const user=this.getUser();
+    if(user==null)
+    {
+      return '';
+    }
+    return user.userId;
+  }
+  static getUserRole():string{
+    const user=this.getUser();
+    if(user==null)
+    {
+      return '';
+    }
+    return user.role;
+  }
 }
